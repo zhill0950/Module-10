4ca9355d94921c18dd4e8483177d4d9cc3f1291f
zhill0950 <zhill0950@gmail.com>
2024-08-13 09:58:51 -0700

    initial commit

---
+++ schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+\c company_db;
+
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+)
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+)
